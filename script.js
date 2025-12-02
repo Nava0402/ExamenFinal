1bfb72df6522b929678f42875d8c0c7389bb7e43
Nava0402 <al45848@un.edu.mx>
2025-12-02 10:14:11 -0600

    avance 3

--- script.js
+++ script.js
@@ -8,12 +8,10 @@ perfilCard.addEventListener("click", function() {
     if (esMasculino) {
         nombre.textContent = "Olivia Sykes";
         ocupacion.innerHTML = "<strong>Ocupación:</strong> Vocalista de la Banda Bring Me The Horizon";
-        alert("Se cambió el perfil a femenino.")
     } else {
         nombre.textContent = "Oliver Sykes";
         ocupacion.innerHTML = "<strong>Ocupación:</strong> Vocalista de la Banda Bring Me The Horizon";
-        alert("Se cambió el perfil a masculino.")
     }
 
     esMasculino = !esMasculino;
-})
+});
